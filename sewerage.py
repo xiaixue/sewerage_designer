c726ac2d6f48b0945fbbac8ea285c4716a8749c2
xiaixue <99562560+xiaixue@users.noreply.github.com>
2022-04-28 23:33:13 -0500

    Update sewerage.py

--- sewerage.py
+++ sewerage.py
@@ -1,5 +1,5 @@
-import random, math, scipy.optimize as sci
-
+import random, math, scipy.optimize as sci, matplotlib.pyplot as plt, xlsxwriter, sys 
+from sample_data import *
 def asker():
   sections, pozos = list(), list()
   print('Ingrese sus sus secciones con este formato separadas por un espacio.')
@@ -126,7 +126,7 @@ class Sewerage:
       
       #	Harmon Coeff #
       if i[7] < 1000: M = 3.8
-      elif i[7] > 100000: M = 2
+      elif i[7] > 63450: M = 2.7
       else: M = 1 + 14 / ( 4 + math.sqrt( i[7] / 1000 ) )
       i.append(M)
 
@@ -171,61 +171,6 @@ class Sewerage:
     if len(dtfix) != 0:
       return self.hydAnalysis()
     return self.tramos
-
-  def separationRegulations(self):
-    wrong, fixed = list(), list()
-
-    for i in self.tramos:
-      d = i[14] * 2.54
-
-      if d < 61: s = 100
-      elif 61 <= d < 122: s = 125
-      elif 122 <= d < 305: s = 150
-      else: s = 150
-
-      if i[3] > s:
-        divider = int( round(i[3] / s, 0) )
-        incorrect = ( i , math.ceil(i[3] / s) )
-        wrong.append(incorrect)
-      else: continue
-
-    for x in wrong:
-      # Assigning values #
-      h = x[1]
-      partial = x[0]
-
-      # Properties #
-      new_lenght = partial[3] / ( h )
-      start = str(partial[1])
-      section = partial[0]
-      own = partial[5] / h
-      added = partial[6]
-
-      for i in range(1,h+1):
-        section_fine = list()
-        section_fine.append(section)
-        section_fine.append(start)
-
-        if i == h: start = partial[2]
-        else: start += "'"
-
-        section_fine.append(start)
-        section_fine.append(new_lenght)
-        section_fine.append(partial[4])
-        section_fine.append(own)
-        section_fine.append(added)
-        section_fine.append(own + added)
-        section_fine.append(partial[8])
-        added += own
-
-        fixed.append(section_fine)
-    listilla = list()
-    for k in wrong:
-      self.tramos.remove(k[0])
-      listilla.append(k[0])
-    self.tramos += fixed
-
-    return self.designPipe(), listilla
 
   def hydAnalysis(self):
     self.analysis = list()
@@ -284,48 +229,47 @@ class Sewerage:
     else:
       return self.nivelesArrastre()
 
+
 if __name__ == '__main__':
-  section, manholes = asker()  
+  "Add Velocity check, read excel"
+  # section, manholes = asker()  
   sew = Sewerage(section, manholes, n= 0.01)
-  try:
-    filled = sew.algoFiller()
-  except:
-    import sys; sys.setrecursionlimit(1500); 
-    filled = sew.algoFiller()
+  sys.setrecursionlimit(10000); 
+  filled = sew.algoFiller()
   
   slopes = sew.slope()
   design = sew.designPipe()
   nah    = sew.nivelesArrastre()
   hyd    = sew.hydAnalysis()
-  #_, wrong = sew.separationRegulations()
-  import xlsxwriter
-  workbook = xlsxwriter.Workbook("sewerage.xlsx", {'constant_memory': True})
 
+  workbook = xlsxwriter.Workbook("sewerage.xlsx")
   data_inc  = workbook.add_worksheet('Datos')
   data_cmp  = workbook.add_worksheet('Datos Completos')
   analysis  = workbook.add_worksheet('Analisis Hidraulico ')
   pozosnivl = workbook.add_worksheet('Niveles de Arrastre Hidraulico')
-  incorrect = workbook.add_worksheet('Exceden Longitud')
 
   def writtingExcel(data, wksheet):
+    format = workbook.add_format()
+    format.set_align('center')
+    format.set_align('vcenter')
+    format.set_font_name('Open Sans')
+    format.set_font_size(12)
     if type(data) != type({'s': 3}):
       row = 0
       for x in data:
         col = 0
         for y in x:
-          wksheet.write(row,col, y)
+          wksheet.write(row,col, y, format)
           col += 1
         row += 1
     else:
       row = 0
       for x, y in data.items():
-        wksheet.write(row, 0, x)
-        wksheet.write(row, 1, sew.pozos[x])
-        wksheet.write(row, 2, y)
-        if y < 0:
-          wksheet.write(row, 3, sew.pozos[x] + y * -1)
-        else:
-          wksheet.write(row, 3, sew.pozos[x] - y)
+        wksheet.write(row, 0, x, format)
+        wksheet.write(row, 1, sew.pozos[x], format)
+        wksheet.write(row, 2, y, format)
+        if y < 0: wksheet.write(row, 3, sew.pozos[x] + y * -1, format)
+        else: wksheet.write(row, 3, sew.pozos[x] - y, format)
         row += 1
     return 0
 
@@ -333,5 +277,53 @@ if __name__ == '__main__':
   writtingExcel(sew.tramos, data_cmp)
   writtingExcel(hyd, analysis)
   writtingExcel(nah, pozosnivl)
-
+  
   workbook.close()
+
+  for shit in sew.tramos:
+    if shit[1] == '71':
+      a = shit
+      break
+  memory_mh = list()
+  memory_mh.append(a[1])
+  memory_mh.append(a[2])
+
+  def getter(tramo, mh):
+    end = tramo[2]      
+    while True:
+      rnd_section = random.choice(sew.tramos)
+      if end == rnd_section[1]:
+        mh.append(rnd_section[2])
+        return getter(rnd_section, mh)
+      if end == '26':
+        break
+    return mh
+  
+  omg = getter(a, memory_mh)
+  nivpzo, nivnah = list(), list()
+  for j in omg:
+    nivpzo.append(sew.pozos[j])
+    nivnah.append(sew.new_nah[j])
+  print(omg)
+  print(nivnah)
+  print(nivpzo)
+
+  plt.plot(omg, nivnah, '-.c', label= 'Nivel de Arrastre Hidráulico')
+  plt.plot(omg, nivpzo, '-m', label= 'Nivel del Rasante')
+  plt.legend()
+  plt.show()
+
+"""
+  order = dict()
+
+  for h, i in enumerate(sections_goodie):
+    order[i[0]] = h
+  
+  new_order = list(order)
+  new_order.sort()
+  ordered_sections = list()
+  
+  for x, y in enumerate(new_order):
+    section_index = order[y]; section = sections_goodie[section_index]
+    ordered_sections.append(section)
+"""
